27568bb591ccbe0eb4fe69eda6f92c816ee1ce4b
Rian Asmara Putra <rputra711@gmail.com>
2022-01-19 20:34:24 +0800

    initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "35b416e1c07609c1e33f";
+/******/ 	var hotCurrentHash = "5f5fa2410da86ca94bdc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
